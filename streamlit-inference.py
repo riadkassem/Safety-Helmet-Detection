94360ec74ccd8514080788d12945ae7a22511396
Riad <riad.kassem17@gmail.com>
2025-09-01 02:43:52 +0300

    Minor changes

--- streamlit-inference.py
+++ streamlit-inference.py
@@ -1,7 +1,7 @@
 from ultralytics import solutions
 
 inf = solutions.Inference(
-    model="/home/archlinux/Desktop/Projects/CV/my_model/my_model.pt",  # you can use any model that Ultralytics support, i.e. YOLO11, YOLOv10
+    model="/home/archlinux/Desktop/Projects/CV/Safety_Helmet_Detection/my_model/my_model.pt",  # you can use any model that Ultralytics support, i.e. YOLO11, YOLOv10
 )
 
 inf.inference()
