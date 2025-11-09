30a0efb6fc4cf663efb163a5f124358672723f94
Riad <riad.kassem17@gmail.com>
2025-11-09 06:29:43 +0200

    Added packages.txt

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from ultralytics import solutions
 
-## Change path to where the model.pt is
-inf = solutions.Inference(model="my_model/my_model.pt")
+inf = solutions.Inference(
+    model="my_model.pt",  # you can use any model that Ultralytics support, i.e. YOLO11, YOLOv10
+)
+
 inf.inference()
 
 # Make sure to run the file using command `streamlit run path/to/file.py`
