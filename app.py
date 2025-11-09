91903805c7e41009a676ee25f9c3d10ef59be15a
Riad <riad.kassem17@gmail.com>
2025-11-09 06:24:48 +0200

    Added packages.txt

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from ultralytics import solutions
 
-inf = solutions.Inference(
-    model="/home/archlinux/Desktop/Projects/CV/Safety_Helmet_Detection/my_model/my_model.pt",  # you can use any model that Ultralytics support, i.e. YOLO11, YOLOv10
-)
-
+## Change path to where the model.pt is
+inf = solutions.Inference(model="my_model/my_model.pt")
 inf.inference()
 
 # Make sure to run the file using command `streamlit run path/to/file.py`
